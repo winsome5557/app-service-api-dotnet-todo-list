fb0495d526b7bf292d23bc0db944a7709a3975d8
Sarfraz Malik <SarfrazMalik@tfl.gov.uk>
2017-01-26 16:35:14 +0000

    fixed issues

---
+++ src/ToDoListDataAPI/Controllers/StationsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using TNetWebApi.Respository;
+
+namespace TNetWebApi.Controllers
+{
+    public class StationsController : ApiController
+    {
+        static StationsRepository repository;
+
+         static StationsController()
+        {
+            repository = new StationsRepository();
+        }
+
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<Station> Get()
+        {
+            return repository.GetStations();
+        }
+
+        // GET api/values/5
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public Station Get(int id)
+        {
+            return repository.GetStationByID(id);
+        }
+
+        // POST api/values
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void Post([FromBody]string value)
+        {
+            repository.AddStation(value);
+        }
+
+        // PUT api/values/5
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Put(int id, [FromBody]string value)
+        {
+            repository.UpdateStation(id, value);
+        }
+
+        // DELETE api/values/5
+        [SwaggerOperation("Delete")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Delete(int id)
+        {
+            repository.RemoveStation(id);
+        }
+    }
+}
---
+++ src/ToDoListDataAPI/StationsRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TNetWebApi.Respository
+{
+
+    public class Station
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    ///  Class for repository
+    /// </summary>
+    public class StationsRepository
+    {
+        public List<Station> StationNames { get; set; }
+
+
+
+        public StationsRepository()
+        {
+            StationNames = new List<Station>() { new Station() { ID=1, Name="Hillingdon" }, new Station() { ID = 1, Name="Uxbridge" }, new Station() { ID = 1, Name="Ickenham" }, new Station() { ID = 1, Name="Riuslip" } };
+        }
+
+
+        public IEnumerable<Station> GetStations()
+        {
+            return StationNames;
+        }
+
+        public IEnumerable<Station> AddStation(string station, int id)
+        {
+            if(!StationNames.Exists(s=>s.ID.Equals(id)))
+            {
+                StationNames.Add(new Station() { ID= id,Name= station});
+            }
+            return StationNames; 
+        }
+
+        public Station GetStationByID(int id)
+        {
+            Station station = StationNames.Where(s => s.ID.Equals(id)).FirstOrDefault();
+            return station;
+        }
+
+        internal void AddStation(string value)
+        {
+            // get max id
+            int maxId = StationNames.Select(s => s.ID).ToList().Max();
+            StationNames.Add(new Station { ID = maxId + 1, Name = value });
+        }
+
+        public IEnumerable<Station> RemoveStation(int Id)
+        {
+            Station objToRem = StationNames.Find(s => s.ID.Equals(Id));
+            if(objToRem != null)
+            {
+                StationNames.Remove(objToRem);
+            }
+            return StationNames; 
+        }
+
+
+        public IEnumerable<Station> UpdateStation(int Id, string value)
+        {
+            Station objToUpdate = StationNames.Find(s => s.ID.Equals(Id));
+            
+            if (objToUpdate != null)
+            {
+                StationNames.Remove(objToUpdate);
+                objToUpdate.Name = value;
+                StationNames.Add(objToUpdate);
+            }
+
+            return StationNames;
+        }
+
+    }
+}
